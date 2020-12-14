703ca5599583031d6a8777367cc13af3e605f90a
samfrexz <samueloyawale315@gmail.com>
2020-12-14 15:53:48 +0100

    Next commit

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+     <h1>{{ title }}</h1>
+   <ul>
+
+     <li v-bind:key="book.title" v-for="book in books">
+      Title: <b>{{ book.title }}</b> By: <b>{{ book.author }}</b>
+     </li>
+   </ul>
+    <BookForm @addBook='appendBook' />
+  </div>
+</template>
+
+<script>
+import BookForm from './components/BookForm.vue';
+
+export default {
+  name: 'App',
+  components: {
+    BookForm
+  },
+  
+   data() {
+    return {
+      title: 'All Books',
+       books: [
+                {title: 'Self-Reliance', author:'Ralph Waldo Emerson'},
+                {title: 'American Gods', author:'Niel Gaiman'},
+                {title: 'Amusing Ourselves to Death', author:'Niel Postman'},
+                {title: 'The Chronicles of Simplified coding:', author:'Samuel Damilola'},
+                {title: 'Coding Made Simple', author:'Samfrexz'},
+            ]
+    }
+  },
+   methods:{
+        appendBook(bookTitle, bookAuthor){
+            this.books.push({title: bookTitle, author: bookAuthor})
+        }
+    },
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+ul {
+   list-style-type: none;
+ }
+ 
+</style>
---
+++ src/components/BookForm.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <form v-on:submit.prevent="bookSubmit(bookTitle, bookAuthor)">
+            <input  v-model="bookTitle" type="text" name="bookTitle" placeholder="Enter your book title">
+            &nbsp;<input v-model="bookAuthor" type="text" name="bookAuthor" placeholder="Enter your book author">
+           &nbsp; <input type="submit" value="Add Book">
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BookForm',
+    props: ['books'],
+    data() {
+        return {
+            bookTitle: '',
+            bookAuthor: '',
+        }
+    },
+     methods:{
+        bookSubmit(bookTitle, bookAuthor){
+            this.$emit('addBook', bookTitle, bookAuthor)
+        }
+    }
+    
+}
+</script>
+
+<style scoped>
+input[type=submit] {
+    padding: 8px 15px;
+    background: black;
+    color: white;
+    border: none;
+    cursor: pointer;
+}
+input[type=text] {
+    padding: 7px 21px;
+
+}
+</style>
